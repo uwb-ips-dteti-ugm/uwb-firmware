bf3ff09dced25ae9a7ca46b77f6556ee73af7146
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2026-05-20 23:40:57 +0700

    refactor/total/codex: (test) Hello world test.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
+#include <Arduino.h>
 #include "composition/app.h"
 
 void setup()
 {
-    static composition::App app;
-    app.run();
+    Serial.begin(115200);
+    delay(1000);
+    // static composition::App app;
+    // app.run();
 }
 
 void loop()
 {
+    Serial.println("Hello, world!");
+    delay(1000);
 }
